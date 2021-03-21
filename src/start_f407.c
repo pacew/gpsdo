163748af734f254f66054e4d5a944822bc305cde
Pace Willisson <pace@alum.mit.edu>
2021-03-21 18:13:10 -0400

    init

--- src/start_f407.c
+++ src/start_f407.c
@@ -104,27 +104,22 @@ swo_putc (int c)
 	ITM_STIM0 = c;
 }
 
+int delay_speed = 500 * 1000;
 
 void
 small_delay()
 {
 	int i;
-	for (i = 0; i < 1000000; i++) {
+	for (i = 0; i < delay_speed; i++) {
 		__asm__("nop");
 	}
 }
 
-/*
- * vectors.S contains the hardware vector table and is placed at the
- * start of the flash image.  The first word points to _estack, a
- * symbol defined in ./ldscript to be the end of internal SRAM, and
- * this pointer is loaded into the stack pointer by the processor
- * reset sequence.  The second word points directly at the c function
- * "start", defined here.
- */
 void
-start (void)
+blinker (void)
 {
+	delay_speed = 50000;
+
 	/* LED_STATUS PA3 */
 	RCC_AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 	small_delay ();
@@ -138,6 +133,43 @@ start (void)
 		GPIOA_BSRR = (1 << (3+16));
 		small_delay();
 	}
+}
+
+
+extern unsigned char _sidata[], _sdata[], _edata[];
+extern unsigned char _sbss[], _ebss[];
+
+/*
+ * vectors.S contains the hardware vector table and is placed at the
+ * start of the flash image.  The first word points to _estack, a
+ * symbol defined in ./ldscript to be the end of internal SRAM, and
+ * this pointer is loaded into the stack pointer by the processor
+ * reset sequence.  The second word points directly at the c function
+ * "start", defined here.
+ */
+void
+start (void)
+{
+	/* 
+	 * the data segment is full of junk when we get here,
+	 * so don't call out anywhere before the initialization
+	 * immediately below.  gcc can insert a call
+	 * memset(3) behind your back if you declare an
+	 * initialized local array, so don't do anything here
+	 * but the data and bss init, then call main() or some
+	 * other function to carry on.
+	 */
+
+	/* clear the bss (uninitalized global variables) */
+	for (int i = 0; i < _ebss - _sbss; i++)
+		_sbss[i] = 0;
+
+	/* copy block of data for initialized variables from flash to ram */
+	for (int i = 0; i < _edata - _sdata; i++)
+		_sdata[i] = _sidata[i];
+
+
+	blinker ();
 }
 
 void
