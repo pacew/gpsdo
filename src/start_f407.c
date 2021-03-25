36a86fc2500b2de9d0ae950a65f97127d32afd92
Pace Willisson <pace@alum.mit.edu>
2021-03-24 22:03:47 -0400

    gps

--- src/start_f407.c
+++ src/start_f407.c
@@ -2,13 +2,97 @@
 
 #include "printf.h"
 
-static void setup_usart3 (void);
-int gps_getc (void);
-
-
 double hclk_hz = 16e6;
 double apb1_hz = 4e6;
 double apb2_hz = 8e6;
+
+
+/* see DDI0489B_cortex_m7_trm.pdf and DDI0403E_B_armv7m_arm.pdf */
+#define SYST_CSR (*(unsigned int volatile *)0xE000E010)
+#define SYST_RVR (*(unsigned int volatile *)0xE000E014)
+#define SYST_CVR (*(unsigned int volatile *)0xE000E018)
+#define SYST_CALIB (*(unsigned int volatile *)0xE000E01C)
+static int systicks_per_10ms = 0;
+
+/*
+ * call this after changing clock speed
+ *
+ * sets up systick to be freerunning 24 bit downcounter
+ * at ahb_hz/8 ticks per second. figure on it wrapping
+ * about once a minue
+ */
+static void
+systick_setup (void)
+{
+	SYST_RVR = 0xffffff; // reload register
+	SYST_CVR = 0; // current count
+
+	/* enable, use ahb_hz / 8 clock, no interrupt */
+	SYST_CSR = (SYST_CSR & ~7) | 1;
+
+	systicks_per_10ms = hclk_hz / 8 / 100;
+}
+
+#define systick_read() (SYST_CVR & 0x00ffffff)
+
+float
+systick_delta_secs (unsigned int end, unsigned int start)
+{
+	unsigned int delta_ticks = (start - end) & 0x00ffffff;
+	return ((float)delta_ticks / (systicks_per_10ms * 100));
+}
+
+float
+systick_secs (unsigned int start)
+{
+	unsigned int delta_ticks = (start - systick_read()) & 0x00ffffff;
+	return ((float)delta_ticks / (systicks_per_10ms * 100));
+}
+
+void
+busywait_ticks (int ticks)
+{
+	unsigned int start = SYST_CVR & 0xffffff;
+
+	if ((SYST_CSR & 1) == 0)
+		systick_setup ();
+	
+	while (1) {
+		unsigned int delta = (start - SYST_CVR) & 0x00ffffff;
+		if (delta >= ticks)
+			break;
+	}
+}
+
+void
+busywait_ms (int ms)
+{
+	int togo = ms;
+	
+	/* simple strategy with no overflow worries */
+	while (togo >= 10) {
+		busywait_ticks (systicks_per_10ms);
+		togo -= 10;
+	}
+	busywait_ticks (togo * (systicks_per_10ms + 9) / 10);
+}
+
+void
+busywait_us (int us)
+{
+	int togo = us;
+	
+	/* simple strategy with no overflow worries */
+	while (togo >= 10000) {
+		busywait_ticks (systicks_per_10ms);
+		togo -= 10000;
+	}
+	busywait_ticks (togo * (systicks_per_10ms + 9999) / 10000);
+}
+
+static void setup_usart3 (void);
+int gps_getc (void);
+
 
 void small_delay(void);
 
@@ -154,6 +238,19 @@ small_delay()
 }
 
 void
+pb11_setup (void)
+{
+	RCC_AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+}
+
+
+int
+pb11_read (void)
+{
+	return (0);
+}
+
+void
 blinker (void)
 {
 	delay_speed = 50000;
@@ -164,17 +261,33 @@ blinker (void)
 
 	GPIOA_MODER = (GPIOA_MODER & ~0xc0) | 0x40; // output
 
+	systick_setup ();
+
+	//pb11_setup ();
 	setup_usart3 ();
+
+	unsigned int last = systick_read();
 
 	int c = 0;
 	while (1) {
-		GPIOA_BSRR = (1 << 3);
-		small_delay();
+		if (systick_secs (last) >= 1) {
+			int a, b, c;
+			a = USART3_SR;
+			USART3_SR &= ~0x100;
+			b = USART3_DR;
+			c = USART3_SR;
+			printf ("tick %x %x %x\n", a, b, c);
+			last = systick_read ();
+		}
 
-		GPIOA_BSRR = (1 << (3+16));
-		small_delay();
+		if (0) {
+			GPIOA_BSRR = (1 << 3);
+			small_delay();
 
-		swo_putc('x');
+			GPIOA_BSRR = (1 << (3+16));
+			small_delay();
+		}
+
 		if ((c = gps_getc ()) >= 0)
 			swo_putc (c);
 	}
@@ -224,89 +337,6 @@ intr_unhandled (void)
 {
 	while(1)
 		;
-}
-
-/* see DDI0489B_cortex_m7_trm.pdf and DDI0403E_B_armv7m_arm.pdf */
-#define SYST_CSR (*(unsigned int volatile *)0xE000E010)
-#define SYST_RVR (*(unsigned int volatile *)0xE000E014)
-#define SYST_CVR (*(unsigned int volatile *)0xE000E018)
-#define SYST_CALIB (*(unsigned int volatile *)0xE000E01C)
-static int systicks_per_10ms = 0;
-
-/*
- * call this after changing clock speed
- *
- * sets up systick to be freerunning 24 bit downcounter
- * at ahb_hz/8 ticks per second. figure on it wrapping
- * about once a minue
- */
-static void
-systick_setup (void)
-{
-	SYST_RVR = 0xffffff; // reload register
-	SYST_CVR = 0; // current count
-
-	/* enable, use ahb_hz / 8 clock, no interrupt */
-	SYST_CSR = (SYST_CSR & ~7) | 1;
-
-	systicks_per_10ms = hclk_hz / 8 / 100;
-}
-
-#define systick_read() (SYST_CVR & 0x00ffffff)
-
-float
-systick_delta_secs (unsigned int end, unsigned int start)
-{
-	unsigned int delta_ticks = (start - end) & 0x00ffffff;
-	return ((float)delta_ticks / (systicks_per_10ms * 100));
-}
-
-float
-systick_secs (unsigned int start)
-{
-	unsigned int delta_ticks = (start - systick_read()) & 0x00ffffff;
-	return ((float)delta_ticks / (systicks_per_10ms * 100));
-}
-
-void
-busywait_ticks (int ticks)
-{
-	unsigned int start = SYST_CVR & 0xffffff;
-
-	if ((SYST_CSR & 1) == 0)
-		systick_setup ();
-	
-	while (1) {
-		unsigned int delta = (start - SYST_CVR) & 0x00ffffff;
-		if (delta >= ticks)
-			break;
-	}
-}
-
-void
-busywait_ms (int ms)
-{
-	int togo = ms;
-	
-	/* simple strategy with no overflow worries */
-	while (togo >= 10) {
-		busywait_ticks (systicks_per_10ms);
-		togo -= 10;
-	}
-	busywait_ticks (togo * (systicks_per_10ms + 9) / 10);
-}
-
-void
-busywait_us (int us)
-{
-	int togo = us;
-	
-	/* simple strategy with no overflow worries */
-	while (togo >= 10000) {
-		busywait_ticks (systicks_per_10ms);
-		togo -= 10000;
-	}
-	busywait_ticks (togo * (systicks_per_10ms + 9999) / 10000);
 }
 
 /* processor specific */
@@ -424,29 +454,36 @@ setup_usart3 (void)
 	/* careful ... different usarts use different clocks */
 	float clock = apb1_hz;
 
-	RCC_APB1ENR |= RCC_APB1ENR_USART3EN; // enable clock
-	
-	/* enable GPIO */
-	RCC_AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOAEN;
+	// enable devices
+	RCC_APB1ENR |= RCC_APB1ENR_USART3EN;
+	RCC_AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
 	busywait_ticks (1);
+
+	printf ("before %x\n", GPIOB_MODER);
 	GPIOB_MODER |= 2<<(10*2); // alternate func for PB10
 	GPIOB_MODER |= 2<<(11*2); // alternate func for PB11
 
 	GPIOB_AFRH |= 7<<((10-8)*4); // PB10 alternate 7
 	GPIOB_AFRH |= 7<<((11-8)*4); // PB11 alternate 7
 
-	USART3_BRR = clock / 9600;
+	/* should be 9600 ... not sure where the factor of 4 is from */
+	USART3_BRR = clock / 2400;
 
 	USART3_CR1 |= USART_CR1_UE; // USART Enable
 	busywait_ticks (1);
 	USART3_CR1 |= USART_CR1_TE | USART_CR1_RE; // xmit & rcv enable
+
+	printf ("RCC_APB1ENR %x\n", RCC_APB1ENR);
+	printf ("GPIOB_MODER %x\n", GPIOB_MODER);
+	printf ("GPIOB_AFRH %x\n", GPIOB_AFRH);
+	printf ("USART3_BRR %x\n", USART3_BRR);
+	printf ("USART3_CR1 %x\n", USART3_CR1);
+
 }
 
 int
 gps_getc (void)
 {
-	return (USART3_DR & 0xff);
-
 	if (USART3_SR & USART_SR_RXNE)
 		return (USART3_DR & 0xff);
 	return (-1);
